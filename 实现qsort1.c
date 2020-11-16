29cede25a5030030f3515a5beffd1c0076ff6e88
ajian2002 <ajian020515@163.com>
2020-11-16 14:03:59 +0800

    s

--- 实现qsort1.c
+++ 实现qsort1.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int *getdata(void *a)
-{ //待更改
-    //printf("%d\n", *(int *)a);
-    return (int *)a;
-}
+#include <string.h>
+int sfds = 0;
 int compare(const void *a, const void *b) //待优化
 {
     return (*(int *)a > *(int *)b);
 }
 void swapany(void *a, void *b, int size) //交换任意类型的数据
 {
+    /*
     int i = 0;
     for (i = 0; i < size; i++)
     {
@@ -19,8 +17,16 @@ void swapany(void *a, void *b, int size) //交换任意类型的数据
         *((char *)a + i) = *((char *)b + i);
         *((char *)b + i) = temp;
     }
+    */
+    char aa[size];
+    char bb[size];
+    void *c;
+    c = memcpy(aa, b, size);
+
+    b = memcpy(b, a, size);
+    a = memcpy(a, c, size);
 }
-void qsort(void *p, int count, int size, int (*compare)(const void *, const void *))
+void qsort(void *p, size_t count, size_t size, int (*compare)(const void *a, const void *b))
 {
     // srand((unsigned int)time(NULL));
     /* 伪快排(像是冒泡)
@@ -36,33 +42,81 @@ void qsort(void *p, int count, int size, int (*compare)(const void *, const void
         }
     }
     */
+    //void *const di = p;
     //摸索
     // int *j=getdata( p;
     //void *k = p + (count - 1) * size;
+    ;
+    //  int cc = count;
+    //    int dd = count;
     if (count > 1)
     {
-        //getdata(p);
-        //void *jj = *(char *)p + size;
-        int jj = 0;
+        int jj = size;
+        // int kk = (count - 1) * size;
+        int c = (rand() % count) * size;
+        //printf("%d ", c / size);
+        void *pp = (char *)p + c;
+        swapany((char *)pp, (char *)p, size);
+        //int cc = count;
+        // int dd = count;
+        //int jj = size;
+        //int kk = (count - 1) * size;
 
-        for (int i = 0; i <= size * count; i += size)
-            if (compare(p, p + i) > 1)
+        /* while (true)
+        {
+            while (compare((char *)p, (char *)p + jj) < 0)
             {
-                swapany(p + i, p + jj, size);
+                jj += size;
+                if (jj == kk)
+                {
+                    break;
+                    //break;
+                }
+
+                //printf("1");
+            }
+            while (compare((char *)p, (char *)p + kk) > 0)
+            {
+                kk -= size;
+                if (kk == jj)
+                {
+                    break;
+                    // break;
+                }
+
+                //  printf("2");
+            }
+            swapany((char *)p + jj, (char *)p + kk, size);
+
+            if (jj >= kk)
+                break;
+            jj += size;
+            printf("3");
+            //printf("2");
+        }
+        */
+        //int jj = size;
+        for (int i = size; i < size * count; i += size)
+        {
+
+            if (compare((char *)p, (char *)p + i) > 0)
+            {
+
+                swapany((char *)p + i, (char *)p + jj, size);
                 jj += size;
             }
-
-        //swapany(p, p + jj, size);
-
+            //printf("q");
+        }
+        swapany((char *)p, (char *)p + jj, size);
+        //printf("%d", sfds++);
         // for (int i = 0; i < count * size; i += size)
         //   printf("%d ", getdata(p + i));
-
-        /*  for (int i = 0; i <= count * size; i += size)
-            printf(" %d ", getdata((char *)p + i));
+        /*    for (int i = 0; i < 10 * size; i += size)
+            printf(" %d ", *((int *)((char *)di + i)));
         printf("\n");
 */
-        qsort(p + jj, count - jj / size, size, compare);
-        qsort(p, jj / size, size, compare);
+        qsort((char *)p, jj / size, size, compare);
+        qsort((char *)p + jj + size, count - jj / size - 1, size, compare);
     }
 }
 
@@ -70,25 +124,27 @@ int main()
 {
     //srand((unsigned int)time(NULL));
     //int a[NUM] = {0};
-    /*
-    int i = 0, n = 0;
+    int i = 0, n = 10;
     //printf("输入数目个数");
-    scanf("%d", &n);
+    //scanf("%d", &n);
     int a[n];
     for (i = 0; i < n; i++)
         scanf("%d", &a[i]);
-*/
+
     //int l = 0;
     //int r = n - 1;
-    int n = 10;
+    /* int n = 10;
     int a[n];
     srand((unsigned)time(NULL));
     for (int i = 0; i < n; i++)
         a[i] = rand() % 10;
-    for (int i = 0; i < n; i++)
+   */
+    /* for (int i = 0; i < n; i++)
         printf(" %d ", a[i]);
-    printf("\n");
+     printf("\n");
+    */
     qsort(a, sizeof(a) / sizeof(a[0]), sizeof(a[0]), compare);
+    printf("\n");
     for (int i = 0; i < n; i++)
         printf(" %d ", a[i]);
     //getchar();

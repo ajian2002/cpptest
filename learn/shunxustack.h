004ecc6eb898c72aa78eb1ef47d9685bcd2dbaf1
ajian2002 <ajian020515@163.com>
2020-12-21 01:08:03 +0800

    z

--- learn/shunxustack.h
+++ learn/shunxustack.h
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAXSIZE 50
 //顺序存储结构 栈的基本操作
-
+typedef struct
+{
+    int data[MAXSIZE];
+    int top;
+}stack;
 //初始化
+void* init_stack(void*head, int size);
 
 //销毁
 
@@ -19,4 +25,3 @@
 //获取个数
 
 //查看栈
-

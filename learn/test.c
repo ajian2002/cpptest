fe80217fab3d3c432834e833c884b657ecf10206
ajian2002 <ajian020515@163.com>
2020-12-28 00:58:50 +0800

    asd

--- learn/test.c
+++ learn/test.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 #include <string.h>
+// #include "sinlinklist1.h"
+#include "sinlinklist1.h"
+#include "../leecode/2.两数相加.c"
 int main()
 {
-    char s[]="[400"
-strchr(&s[i+1],',')-&s[i+1];
+    struct ListNode *head = creaklink(1, 9);
+    head = pushlink(head, 9);
+    head = pushlink(head, 9);
+    head = pushlink(head, 9);
+    head = pushlink(head, 9);
+    struct ListNode *head2 = creaklink(1, 9);
+    head2 = pushlink(head2, 9);
+    head2 = pushlink(head2, 9);
+    //输入
+    seelink(head);
+    seelink(head2);
+    struct ListNode *head3 = addTwoNumbers(head, head2);
+    seelink(head3);
+    dellink(head3);
+    //dellink(head);
+    //dellink(head2);
 }

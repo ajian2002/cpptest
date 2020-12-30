213e13342d0c5e66d99c02e4878015d3ef9d75af
ajian2002 <ajian020515@163.com>
2020-12-30 21:08:24 +0800

    sd

---
+++ luogu/P5744.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+typedef struct
+{
+    char name[20];
+    int age;
+    int sum;
+} node;
+node *dep(node *a)
+{
+    a->age++;
+    a->sum = (a->sum) * 1.2;
+    if (a->sum > 600)
+        a->sum = 600;
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    node student[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%s%d%d", student[i].name, &student[i].age, &student[i].sum);
+
+        dep(&student[i]);
+
+        printf("%s %d %d\n", student[i].name, student[i].age, student[i].sum);
+    }
+}

272cb44caf10280b25665ab8eec4559b8b0be9aa
ajian2002 <ajian020515@163.com>
2021-03-07 23:02:41 +0800

    a

---
+++ leecode/745.前缀和后缀搜索.c
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=745 lang=c
+ *
+ * [745] 前缀和后缀搜索
+ */
+
+// @lc code=start
+
+
+
+typedef struct {
+    
+} WordFilter;
+
+
+WordFilter* wordFilterCreate(char ** words, int wordsSize) {
+    
+}
+
+int wordFilterF(WordFilter* obj, char * prefix, char * suffix) {
+  
+}
+
+void wordFilterFree(WordFilter* obj) {
+    
+}
+
+/**
+ * Your WordFilter struct will be instantiated and called as such:
+ * WordFilter* obj = wordFilterCreate(words, wordsSize);
+ * int param_1 = wordFilterF(obj, prefix, suffix);
+ 
+ * wordFilterFree(obj);
+*/
+// @lc code=end
+
--- others/网址提取.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main()
-{
-    char a[100] = {"http://www.1xffffff.org/2012/11/20/1-gdb-coredump/"};
-    int b[4] = {2012, 11, 20};
-    printf("%d-%d-%d\n", b[1], b[2], b[0]);
-}

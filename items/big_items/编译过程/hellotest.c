be07aa1f6a7b5f550cf76e24d48cc72623ae6f61
AutoupdateRobot <AutoupdateRobot@email.com>
2021-03-11 17:05:51 +0800

    chore(none): md文件分类

---
+++ items/big_items/编译过程/hellotest.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#define DEBUG
+//#undef DEBUGPRINT
+#include <my/debug.info.h>
+
+int main()
+{
+	char *s = "abc";
+	//  DEBUGPRINT("123");
+	//DEBUGPRINT("%s", s);
+	//
+	if (1)
+
+		WRITE_LOG("log","123%s""\n",s);
+	//PRINTEXIT("p || g is NULL \n");
+
+	//PRINTEXIT("%s", s);
+	//printf("%s\n");
+
+	//s[0] = '\0';
+	//printf("%s\n", s);
+
+	return 0;
+}

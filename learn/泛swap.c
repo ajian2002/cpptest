f5ab55b882a17b2863de91f0b755b44469beae03
AutoupdateRobot <AutoupdateRobot@email.com>
2021-03-20 11:59:30 +0800

    a

---
+++ learn/泛swap.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+void aswap(void *a, void *b, int size)
+{
+    char temp;
+    char *t,*p;
+    t=(char*)a;p=(char*)b;
+    for (int i = 0; i < size; i++, t++,p++)
+    {
+        temp = *t;
+        *t= *p;
+        *p = temp;
+        
+    }
+}
+int main()
+{
+    int a, b;
+    a = 0;
+    b = 1;
+    double c, d;
+    c = 1.0, d = 2.0;
+    aswap(&a, &b, sizeof(int));
+    printf("%d %d\n", a, b);
+    aswap(&c, &d, sizeof(double));
+    printf("%f %f", c, d);
+    return 0;
+}

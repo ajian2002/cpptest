64aed7ac134addebe0a152884f1c1abbca59d7df
ajian2002 <ajian020515@163.com>
2021-04-06 17:48:49 +0800

    a

---
+++ learn/mutextest.c
@@ -0,0 +1,57 @@
+//#define __USE_MISC
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <string.h>
+#include <wait.h>
+#include <errno.h>
+#include <pthread.h>
+//#include <pthread_mytex_t.h>
+#define MYERROR
+#include <my/debug.info.h>
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+int i = 0;
+void *twothread(void *arg)
+{
+    int *mmoney = (int *)arg;
+    do
+    {
+        pthread_mutex_lock(&lock);
+        // printf("    %d\n", i++);
+        *mmoney -= 10;
+
+        pthread_mutex_unlock(&lock);
+    } while (0);
+    //pthread_exit(NULL);
+    return NULL;
+}
+int main()
+{
+    pthread_t pth;
+    int j = 0;
+    int money = 300;
+    pthread_mutex_init(&lock, NULL);
+
+    pthread_create(&pth, NULL, (void *)twothread, (void *)&money);
+
+    while (j++ < 5)
+    {
+        pthread_mutex_lock(&lock);
+        money += 1;
+        printf("%d    \n", money);
+        pthread_mutex_unlock(&lock);
+    
+    }
+    
+    // sleep(1);
+    printf("before %d    \n", money);
+    pthread_join(pth, NULL);
+    printf("join after%d    \n", money);
+    pthread_mutex_destroy(&lock);
+}

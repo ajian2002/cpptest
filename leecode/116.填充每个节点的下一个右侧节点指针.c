f2a7814040dbcbe84ddbb65c6ef98c365db0d06d
ajian2002 <ajian020515@163.com>
2021-04-15 12:14:47 +0800

    tree

--- leecode/116.填充每个节点的下一个右侧节点指针.c
+++ leecode/116.填充每个节点的下一个右侧节点指针.c
@@ -14,18 +14,22 @@
  *     struct Node *next;
  * };
  */
-void help(struct TreeNode *node)
+void help(struct Node *node)
 {
     if (node == NULL || node->left == NULL)
         return;
     else
     {
-        node->left->next = node->right;
 
-        if (node->right == NULL || node->left->right == NULL)
-            ;
-        else
-            node->left->right->next = node->right->left;
+        struct Node *ltemp = node->left;
+        struct Node *rtemp = node->right;
+        ltemp->next = rtemp;
+        while (ltemp->right != NULL && rtemp != NULL)
+        {
+            ltemp->right->next = rtemp->left;
+            ltemp = ltemp->right;
+            rtemp = rtemp->left;
+        }
     }
     help(node->left);
     help(node->right);

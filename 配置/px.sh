09ad2e40c9a7a9f09e1782af6888efffeb514696
ajian2002 <ajian020515@163.com>
2021-04-19 10:16:45 +0800

    a

--- 配置/px.sh
+++
@@ -1,3 +0,0 @@
-export hostIP=`grep -oP  "(\d+\.)+(\d+)" /etc/resolv.conf`
-sudo sed -i "115c http $hostIP 10081" /etc/proxychains4.conf
-sudo sed -i "116c socks5 $hostIP 10080" /etc/proxychains4.conf

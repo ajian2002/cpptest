a03584aa9672372472891d238bca4c6ba5216fb2
ajian2002 <ajian020515@163.com>
2021-04-20 22:17:41 +0800

    a

--- learn/泛型选择.c
+++ learn/泛型选择.c
@@ -9,7 +9,7 @@ int main()
 {
     //double x=1;
     void*x=NULL;
-    if(strcmp("int",GETTYPE(x))==0)
+ if(strcmp("int",GETTYPE(x))==0)
     printf("%d\n",x);
     else if(strcmp("double",GETTYPE(x))==0)
     printf("%f\n",x);

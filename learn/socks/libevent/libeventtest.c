769b1820df4a68730879e1bfb7e3003a6e3ac29c
ajian2002 <ajian020515@163.com>
2021-05-13 20:09:09 +0800

    a

--- learn/socks/libevent/libeventtest.c
+++ learn/socks/libevent/libeventtest.c
@@ -2,12 +2,19 @@
 #include <event.h>
 #include <event2/bufferevent.h>
 #define PORT 5678
+void lfdaccept(int a, short b, void *c)
+{
+    return;
+}
 int main()
 {
     struct event_base *base = event_base_new();
 
     int lfd = socket(AF_INET, SOCK_STREAM, 0);
     {
+    int lfd; //lfd
+    {
+        lfd = socket(AF_INET, SOCK_STREAM, 0);
         struct sockaddr_in addr;
         addr.sin_family = AF_INET;
         addr.sin_addr.s_addr = htons(INADDR_ANY);
@@ -17,4 +24,8 @@ int main()
     }
     bufferevent_socket_new(base, lfd, BEV_OPT_CLOSE_ON_FREE);
     
+
+    //创建事件
+    struct event *lfdevent = event_new(base, lfd, EV_PERSIST, lfdaccept, NULL);
+    // struct event *lfdevent =bufferevent_socket_new(base, lfd, BEV_OPT_CLOSE_ON_FREE);
 }

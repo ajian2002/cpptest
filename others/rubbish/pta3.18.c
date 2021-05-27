06eaae6aecf9e604b1a3eeb89ccfb406df5c81e8
ajian2002 <ajian020515@163.com>
2021-05-27 14:48:55 +0800

    5.27pta

--- others/rubbish/pta3.18.c
+++ others/rubbish/pta3.18.c
@@ -1,38 +1,210 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-int compare(char *a, char *b)
+
+char leapYear(int year)  //闰年否
 {
-    return (*a > *b);
-}
-int main()
-{
-    int n, m;
-    int count = 0;
-    char buf[10000];
-    memset(buf, 0, sizeof(char) * 10000);
-    // scanf("%s", buf);
-    // getline(buf, 10000, stdin);
-    fgets(buf, 10000, stdin);
-    buf[strlen(buf) - 1] = 0;
-    qsort(buf, strlen(buf), sizeof(char), compare);
-    int len = strlen(buf);
-    char *temp = &buf[0];
-    printf("begin:%s\n", buf);
-    for (int i = 1; i < strlen(buf); i++)
+    char IF;
+
+    if (year % 100 == 0)
     {
-        if (strncmp(&buf[i], temp, 1) == 0)
+        if (year % 400 == 0)
+            IF = '1';
+        else
+            IF = '0';
+        goto cww;
+    }
+    else if (year % 4 == 0)
+        IF = '1';
+    else
+        IF = '0';
+cww:
+    return IF;
+}
+struct DateG
+{
+    int yy, mm, dd;
+};
+struct DateG days2Date(int x)
+{
+    // from:{1,1,1} + 100-->{1,4,11}
+    struct DateG mm = {1, 1, 1};
+    int temp        = x;
+    while (1)
+    {  // 31+28+31+10==100+1
+        if ('1' == leapYear(mm.yy) && temp >= 366)
         {
-            int s = strlen(buf);
-            memmove(temp, &buf[i], strlen(buf) - i);
-            buf[s - 1] = 0;
-            i--;
-            printf("%s\n", buf);
+            mm.yy += 1;
+            temp -= 366;
+        }
+        else if ('0' == leapYear(mm.yy) && temp >= 365)
+        {
+            mm.yy += 1;
+            temp -= 365;
         }
         else
         {
-            temp = &buf[i];
+            break;
         }
     }
-    printf("final:%s", buf);
+    int i = mm.mm;
+    while (1)
+    {  // 31+28+31+10==100+1
+
+        if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 ||
+            i == 12)
+        {
+            if (temp >= 31)
+            {
+                i += 1;
+                if (i == 13)
+                {
+                    i = 1;
+                    mm.yy += 1;
+                }
+                temp -= 31;
+            }
+            else
+            {
+                mm.mm = i;
+                break;
+            }
+        }
+        else if (i == 2)
+        {
+            if ('1' == leapYear(mm.yy))
+            {
+                if (temp >= 29)
+                {
+                    i += 1;
+                    temp -= 29;
+                }
+                else
+                {
+                    mm.mm = i;
+                    break;
+                }
+            }
+            else
+            {
+                if (temp >= 28)
+                {
+                    i += 1;
+                    temp -= 28;
+                }
+                else
+                {
+                    mm.mm = i;
+                    break;
+                }
+            }
+        }
+        else if (i == 4 || i == 6 || i == 9 || i == 11)
+        {
+            if (temp >= 30)
+            {
+                i += 1;
+                temp -= 30;
+            }
+            else
+            {
+                mm.mm = i;
+                break;
+            }
+        }
+        else
+        {
+            mm.mm = i;
+            break;
+        }
+    }
+
+    mm.dd += temp;
+    if (mm.mm == 2)
+    {
+        if ('1' == leapYear(mm.yy))
+        {
+            if (mm.dd > 29)
+            {
+                mm.dd %= 29;
+                mm.mm += 1;
+            }
+        }
+        else
+        {
+            if (mm.dd > 28)
+            {
+                mm.dd %= 28;
+                mm.mm += 1;
+            }
+        }
+    }
+    else if (mm.mm == 4 || mm.mm == 6 || mm.mm == 9 || mm.mm == 11)
+    {
+        if (mm.dd > 30)
+        {
+            mm.dd %= 30;
+            mm.mm += 1;
+        }
+    }
+    else
+    {
+        if (mm.dd > 31)
+        {
+            mm.dd %= 31;
+            mm.mm += 1;
+        }
+    }
+
+    return mm;
+}
+int Days(struct DateG x)
+{
+    int sum = 0;
+    for (int i = 1; i < x.yy; i++)
+    {
+        if ('1' == leapYear(i))
+        {
+            sum += 366;
+        }
+        else
+        {
+            sum += 365;
+        }
+    }
+    for (int i = 1; i < x.mm; i++)
+    {
+        if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 ||
+            i == 12)
+        {
+            sum += 31;
+        }
+        else if (i == 2)
+        {
+            if ('1' == leapYear(x.yy))
+            {
+                sum += 29;
+            }
+            else
+            {
+                sum += 28;
+            }
+        }
+        else
+        {
+            sum += 30;
+        }
+    }
+    return sum + x.dd - 1;
+
+}  // {2,,4,11}    return 465   from{1,1,1}
+
+int main()
+{
+    struct DateG mm = {2001, 1, 1};
+    struct DateG nn = {1919, 9, 9};
+    struct DateG xx = days2Date(60 * 365 + Days(mm));
+    printf("%d.%d.%d\n", xx.yy, xx.mm, xx.dd);
+    nn = days2Date(Days(xx) - 1000);
+    printf("%d.%d.%d\n", nn.yy, nn.mm, nn.dd);
+    struct DateG zz = {2020, 1, 23};
+    printf("%.2f\n", (Days(zz) - Days(mm)) / 365.0);
 }
--- others/rubbish/pta5.6.c
+++ others/rubbish/pta5.6.c
@@ -1,50 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-void menu(void)
+int mons[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+int year, mon, day;
+int Isyear(int year)  //判断是否闰年
 {
-	printf("1-Ball\n");
-	printf("2-Cylinder\n");
-	printf("3-Cone\n");
-	printf("other-Exit\n");
-	printf("Please enter your command:\n");
-	return;
+    if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0)
+        return 1;
+    else
+        return 0;
 }
-int main()
+struct DateG days2Date(int x)
 {
-	int m;
-	double r, v, h;
-	r = h = v = 0;
-	while (1)
-	{
-		menu();
-
-		v = 0;
-		scanf("%d", &m);
-		switch (m)
-		{
-		case 1:
-			printf("Please enter the radius:\n");
-			scanf("%lf", &r);
-			//printf("%d %l \n", m, r);
-			v = 4 * 3.14159265 * r * r * r / 3.0;
-			printf("%.2lf\n", v);
-			break;
-		case 2:
-			printf("Please enter the radius and the height:\n");
-			scanf("%lf %lf", &r, &h);
-			v = 3.14159265 * r * r * h;
-			printf("%.2lf\n", v);
-			break;
-		case 3:
-			printf("Please enter the radius and the height:\n");
-			scanf("%lf %lf", &r, &h);
-			v = 3.14159265 * r * r * h / 3.0;
-			printf("%.2lf\n", v);
-			break;
-		default:
-			return 0;
-		}
-	}
-	return 0;
+    struct DateG t = {1, 1, 1};
+    for (int i = 0; i < x; i++)
+    {
+        if (Isyear(t.yy))
+        {
+            mons[1] = 29;
+        }
+        else
+        {
+            mons[1] = 28;
+        }
+        t.dd++;
+        if (t.dd > mons[t.mm - 1])
+        {
+            t.mm++;
+            t.dd = 1;
+            if (t.mm > 12)
+            {
+                t.yy++;
+                t.mm = 1;
+            }
+        }
+    }
+    return t;
+}
+int Days(struct DateG x)  // {2,,4,11}    return 465   from{1,1,1}
+{
+    int year = x.yy, mon = x.mm, day = x.dd;
+    int sum = 0;                    //从1年1月1日到今天的总天数
+    for (int i = 1; i < year; i++)  //计算从开始到去年的年份天数之和
+    {
+        if (Isyear(i))  //闰年加366
+            sum += 366;
+        else  //不是闰年加365
+            sum += 365;
+    }
+    if (Isyear(year))
+    {
+        mons[1] = 29;
+    }
+    else
+    {
+        mons[1] = 28;
+    }
+    for (int i = 0; i < mon - 1; i++)  //加到今年的前一个月的天数和
+        sum += mons[i];
+    sum += day;  //加这个月的天数
+    return sum - 1;
 }
